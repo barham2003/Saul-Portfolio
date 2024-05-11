8d75fafb1b23b73c6a7a48942718e01a6fbca07b
baran6688 <baranali6688@gmail.com>
2024-05-11 10:17:24 +0300

    About Page Done

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,13 @@
 const headerContainerDiv = document.querySelector(".header-container");
-const scrollWatcherDiv = document.createElement("div");
 
+// Creating and putting the watcher element
+const scrollWatcherDiv = document.createElement("div");
 scrollWatcherDiv.setAttribute("data-scroll-watcher", "");
 headerContainerDiv.before(scrollWatcherDiv);
 
+// Making Observation happen
 const navObserver = new IntersectionObserver((enteries) => {
-  console.log(enteries);
   headerContainerDiv.classList.toggle("sticking", !enteries[0].isIntersecting);
 });
-
+// Observe scroll watcher div
 navObserver.observe(scrollWatcherDiv);
