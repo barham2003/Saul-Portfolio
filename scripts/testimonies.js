6325a5a199c23bb040cbb0322eda3e34c05dad59
baran6688 <baranali6688@gmail.com>
2024-05-11 15:48:36 +0300

    http for https

--- scripts/testimonies.js
+++ scripts/testimonies.js
@@ -2,6 +2,6 @@ const videoEmbed = document.querySelector("#embeded");
 const testimonySpan = document.querySelector("#testimony");
 
 function changeVideo(url, name) {
-  videoEmbed.setAttribute("src", `http://www.youtube.com/v/${url}`);
+  videoEmbed.setAttribute("src", `https://www.youtube.com/v/${url}`);
   testimonySpan.textContent = name;
 }
